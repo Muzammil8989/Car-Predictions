6a9575e875b2856a9808527a34bb0cbda6b2e9dc
Muzammil8989 <muzammilkhan89223@gmail.com>
2025-04-11 14:58:01 +0500

    Update

--- app.py
+++ app.py
@@ -3,11 +3,11 @@
 
 import streamlit as st
 import pandas as pd
-import joblib
+import pickle
 import datetime as dt
 
 # Load model
-model = joblib.load('car_price_model.pkl')
+model = pickle.load(open('car_price_model.pkl', 'rb'))
 
 # Custom CSS for modern styling
 st.markdown("""
